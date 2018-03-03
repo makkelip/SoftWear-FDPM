ff457d2b79d58eae0b2f6f3428a60848b55900ec
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-03-03 14:35:52 +0200

    working pageLoad.js

--- WEB/JS/pageLoad.js
+++ WEB/JS/pageLoad.js
@@ -1,8 +1,32 @@
+/*
+If you want button to open a specific view to section.
+Add class to the button formed like: "button" + <verb> + <target>.
+Example: class="buttonViewProject" opens project view form viewProject.html.
+*/
+
 $(document).ready(function(){
-  $(".buttonCreateCustomer").click(function(){
-    $(".grid-section").load("createCustomer.html #create");
+  //READY
+  //Create customer
+  $("body").on("click", ".buttonCreateCustomer", function(){
+    $("section").load("createCustomer.html #createCustomer");
   });
-  $(".buttonSelectCustomer").click(function() {
-    $(".section").load("selectCustomer.html #selectCustomer");
+  //Select customer
+  $("body").on("click", ".buttonSelectCustomer", function() {
+    $("section").load("selectCustomer.html #selectCustomer");
+  });
+
+  //INCOMPLETE
+
+  //View customer
+  $("body").on("click", ".buttonViewCustomer", function() {
+    $("section").load("viewCustomer.html #viewCustomer");
+  });
+  //Create project
+  $("body").on("click", ".buttonCreateCustomer", function() {
+    $("section").load("createProject.html #createProject");
+  });
+  //View Project
+  $("body").on("click", ".buttonViewProject", function() {
+    $("section").load("viewProject.html #viewProject");
   });
 });
