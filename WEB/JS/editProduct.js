53ed19d421d3228c030a271fcc97b05797994aef
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2018-03-08 13:18:18 +0200

    editProduct draft version and reduce code for viewColors.js

---
+++ WEB/JS/editProduct.js
@@ -0,0 +1,33 @@
+$(function(){
+    const colorDisplay = (colors) => {
+        const colorSideBar = document.querySelector('.aside-content');
+        colorSideBar.innerHTML = "";
+        for (let color of colors){
+            colorSideBar.innerHTML +=  `<a class="color-list" id="${color.id}" style="background:${color.hexColorValue}">${color.name}</a>`;
+        }
+        var colorSideBars = document.querySelector('.aside-content');
+        var colorsChosen = colorSideBars.getElementsByTagName("a");
+        colorsChosen.addEventListener('click',function(){
+            document.getElementById("product-colors-text-field").textContent = "";
+        });
+        Array.prototype.forEach.call(colorsChosen, function(colorChosen){
+            colorChosen.addEventListener('click', function(){
+
+                document.getElementById("product-colors-text-field").textContent += `, ${colorChosen.textContent}`;
+            })});
+    }
+        /*
+        var colorChosen = document.getElementsByClassName().value;
+        colorsChosen.color =
+        fetch("http://10.114.32.58:8080/FDPM-SERVER/sources/model.product/1/color/2",{
+            'method': 'PUT',
+            'headers': new Headers({'Content-Type': 'application/json'})
+        }).then(result => result.json())
+        .then(response => console.log('Success', response))
+        .catch(error => console.log('error', error));
+        */
+    fetch("http://10.114.32.58:8080/FDPM-SERVER/sources/model.color")
+        .then(response => response.json())
+    .then(json => colorDisplay(json))
+    .catch(error => console.log(error));
+})
--- WEB/JS/viewColors.js
+++ WEB/JS/viewColors.js
@@ -3,30 +3,20 @@ $(function() {
         console.log(colors);
         const colorsElement = document.querySelector(".color-container-flex");
         colorsElement.innerHTML = "";
-        colorsContainner = new Array();
         if (colorsElement === null){
             throw new Error("No Color yet!");
         } else {
         for (let color of colors) {
             colorsElement.innerHTML +=
                 `<div class="grow color-card"> <!-- värikortti-->
-                    <div class="color"></div> <!-- väri-->
+                    <div class="color" style="background:${color.hexColorValue}"></div> <!-- väri-->
                     <div class="color-info"> <!--väri-info-->
                         <p class="color-info-name">${color.name}</p>
                         <p class="color-info-pantone">${color.pantone}</p>
                         <p class="color-info-hex">${color.hexColorValue}</p>
                     </div>
                 </div>`;
-
-            colorsContainner.push(color.hexColorValue);
         }
-        var colors_display = document.getElementsByClassName('color');
-        console.log(colors_display);
-        console.log(colorsContainner);
-        for (let i = 0; i<colorsContainner.length;i++){
-            colors_display[i].style.backgroundColor = colorsContainner[i];
-        }
-        console.log(colorsElement);
     }
     };
     fetch("http://10.114.32.58:8080/FDPM-SERVER/sources/model.color")
