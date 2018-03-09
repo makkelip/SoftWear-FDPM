1034b5dc98aba6cad4f1dfb3abb129e3a29c03a4
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-03-09 11:56:50 +0200

    change back form one to on in navigation

--- WEB/JS/pageLoad.js
+++ WEB/JS/pageLoad.js
@@ -8,62 +8,62 @@ Example: in viewCustomer.html <div class="viewCustomer"> ... </div>
 
 $(document).ready(function(){
   //Home
-  $("body").one("click", ".js--button-home", function(){
+  $("body").on("click", ".js--button-home", function(){
     $("section").load("index.html #js--home");
   });
 
   //Select customer
-  $("body").one("click", ".js--button-select-customer", function(){
+  $("body").on("click", ".js--button-select-customer", function(){
     $("section").load("selectCustomer.html #js--select-customer");
     $.getScript("JS/selectCustomer.js");
   });
   //Create customer
-  $("body").one("click", ".js--button-create-customer", function(){
+  $("body").on("click", ".js--button-create-customer", function(){
     $("section").load("createCustomer.html #js--create-customer");
     $.getScript("JS/createCustomer.js");
   });
 
   //Select project
-  $("body").one("click", ".js--button-select-project", function(){
+  $("body").on("click", ".js--button-select-project", function(){
     $("section").load("selectProject.html #js--select-project");
     $.getScript("JS/selectProject.js");
   });
   //Create project
-  $("body").one("click", ".js--button-create-project", function(){
+  $("body").on("click", ".js--button-create-project", function(){
     $("section").load("createProject.html #js--create-project");
     $.getScript("JS/createProject.js");
   });
 
   //View colors
-  $("body").one("click", ".js--button-view-colors", function(){
+  $("body").on("click", ".js--button-view-colors", function(){
     $("section").load("viewColors.html #js--all-colors");
     $.getScript("JS/viewColors.js");
   });
 
   //Create color
-  $("body").one("click", ".js--button-create-colors", function(){
+  $("body").on("click", ".js--button-create-colors", function(){
     $("section").load("createColors.html #js--create-colors");
     $.getScript("JS/createColors.js");
   });
 
   //View materials
-  $("body").one("click", ".js--button-view-materials", function(){
+  $("body").on("click", ".js--button-view-materials", function(){
     $("section").load("viewMaterials.html #js--all-materials");
     $.getScript("JS/selectMaterial.js");
     $.getScript("JS/viewMaterials.js")
   });
   //Create materials
-  $("body").one("click", ".js--button-create-materials", function(){
+  $("body").on("click", ".js--button-create-materials", function(){
     $("section").load("createMaterials.html #js--create-materials");
     $.getScript("JS/createMaterials.js");
   });
  //Select product
-  $("body").one("click", ".js--button-select-product", function(){
+  $("body").on("click", ".js--button-select-product", function(){
     $("section").load("selectProduct.html #js--select-product");
     $.getScript("JS/selectProduct.js");
   });
   //Create product
-  $("body").one("click", ".js--button-create-product", function(){
+  $("body").on("click", ".js--button-create-product", function(){
     $("section").load("createProduct.html #js--create-product");
     $.getScript("JS/createProduct.js");
   });
