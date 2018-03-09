dc76ab6f2108d1fd57050490098f7473eb621e24
Jasmin Lehtinen <jasminle@metropolia.fi>
2018-03-09 19:38:03 +0200

    deleting customer works now

--- WEB/JS/deleteCustomer.js
+++ WEB/JS/deleteCustomer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   document.getElementById("js--delete-customer").addEventListener('click', function() {
     deleteCustomer();
-    
+
     //Loads all customers page
     $("section").load("selectCustomer.html #js--select-customer");
     $.getScript("JS/selectCustomer.js");
@@ -14,11 +14,3 @@ function deleteCustomer() {
   })
   .then(response => response.json());
 }
-
-
-
-/*Add delete button to somewhere in viewCustomer.html page!
-
-Add
-$.getScript("JS/deleteCustomer.js");
-to line 12 in viewCustomer.js*/
