22ba12c48412e36d87320e94175a1896009c79eb
makkelip <markus.pikkanen@gmail.com>
2018-03-10 23:15:36 +0200

    navigation buttons single execution added

--- WEB/JS/pageLoad.js
+++ WEB/JS/pageLoad.js
@@ -7,65 +7,88 @@ Example: in viewCustomer.html <div class='viewCustomer'> ... </div>
 */
 
 $(document).ready(function() {
+  const section = $('section');
+  const body = $('body');
   //Home
-  $('body').on('click', '.js--button-home', function() {
-    $('section').load('index.html #js--home');
+  body.on('click', '.js--button-home', function() {
+    if ($('#js--home').length == 0) {
+      section.append('index.html #js--home');
+    }
   });
-
   //Select customer
-  $('body').on('click', '.js--button-select-customer', function() {
-    $('section').load('selectCustomer.html #js--select-customer');
-    $.getScript('JS/selectCustomer.js');
+  body.on('click', '.js--button-select-customer', function() {
+    if ($('#js--select-customer').length == 0) {
+      console.log('load');
+      section.load('selectCustomer.html #js--select-customer');
+      $.getScript('JS/selectCustomer.js');
+    }
   });
   //Create customer
-  $('body').on('click', '.js--button-create-customer', function() {
-    $('section').load('createCustomer.html #js--create-customer');
-    $.getScript('JS/createCustomer.js');
+  body.on('click', '.js--button-create-customer', function() {
+    if ($('#js--create-customer').length == 0) {
+      section.append('createCustomer.html #js--create-customer');
+      $.getScript('JS/createCustomer.js');
+    }
   });
 
   //Select project
-  $('body').on('click', '.js--button-select-project', function() {
-    $('section').load('selectProject.html #js--select-project');
-    $.getScript('JS/selectProject.js');
+  body.on('click', '.js--button-select-project', function() {
+    if ($('#js--select-project').length == 0) {
+      section.load('selectProject.html #js--select-project');
+      $.getScript('JS/selectProject.js');
+    }
   });
   //Create project
-  $('body').on('click', '.js--button-create-project', function() {
-    $('section').load('createProject.html #js--create-project');
-    $.getScript('JS/createProject.js');
+  body.on('click', '.js--button-create-project', function() {
+    if ($('#js--create-project').length == 0) {
+      section.load('createProject.html #js--create-project');
+      $.getScript('JS/createProject.js');
+    }
   });
 
   //View colors
-  $('body').on('click', '.js--button-view-colors', function() {
-    $('section').load('viewColors.html #js--all-colors');
-    $.getScript('JS/viewColors.js');
+  body.on('click', '.js--button-view-colors', function() {
+    if ($('#js--all-colors').length == 0) {
+      section.load('viewColors.html #js--all-colors');
+      $.getScript('JS/viewColors.js');
+    }
   });
   //Create color
-  $('body').on('click', '.js--button-create-colors', function() {
-    $('section').load('createColors.html #js--create-colors');
-    $.getScript('JS/createColors.js');
+  body.on('click', '.js--button-create-colors', function() {
+    if ($('#js--create-colors').length == 0) {
+      section.load('createColors.html #js--create-colors');
+      $.getScript('JS/createColors.js');
+    }
   });
 
   //View materials
-  $('body').on('click', '.js--button-view-materials', function() {
-    $('section').load('viewMaterials.html #js--all-materials');
-    $.getScript('JS/selectMaterial.js');
-    $.getScript('JS/viewMaterials.js');
+  body.on('click', '.js--button-view-materials', function() {
+    if ($('#js--all-materials').length == 0) {
+      section.load('viewMaterials.html #js--all-materials');
+      $.getScript('JS/viewMaterials.js');
+    }
   });
   //Create materials
-  $('body').on('click', '.js--button-create-materials', function() {
-    $('section').load('createMaterials.html #js--create-materials');
-    $.getScript('JS/createMaterials.js');
+  body.on('click', '.js--button-create-materials', function() {
+    if ($('#js--create-materials').length == 0) {
+      section.load('createMaterials.html #js--create-materials');
+      $.getScript('JS/createMaterials.js');
+    }
   });
 
   //Select product
-  $('body').on('click', '.js--button-select-product', function() {
-    $('section').load('selectProduct.html #js--select-product');
-    $.getScript('JS/selectProduct.js');
+  body.on('click', '.js--button-select-product', function() {
+    if ($('#js--select-product').length == 0) {
+      section.load('selectProduct.html #js--select-product');
+      $.getScript('JS/selectProduct.js');
+    }
   });
   //Create product
-  $('body').on('click', '.js--button-create-product', function() {
-    $('section').load('createProduct.html #js--create-product');
-    $.getScript('JS/createProduct.js');
+  body.on('click', '.js--button-create-product', function() {
+    if ($('#js--create-product').length == 0) {
+      section.load('createProduct.html #js--create-product');
+      $.getScript('JS/createProduct.js');
+    }
   });
 
 });
